92cfd62d00defbfbc750c4b489b7c9a7c0089cda
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-17 18:43:44 +0000

    Create resident entity

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/entity/Resident.java
@@ -0,0 +1,71 @@
+
+package com.jackalcode.resident_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "residents")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Resident {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "admission_date", nullable = false)
+    private LocalDate admissionDate;
+
+    @Column(name = "primary_phone_number")
+    private String primaryPhoneNumber;
+
+    @Column(name = "nhs_number")
+    private String nhsNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String allergies;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
