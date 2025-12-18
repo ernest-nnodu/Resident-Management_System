66e401e37fa86b219941026e8ce6c2f1bb59b117
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-18 18:47:29 +0000

    Add create resident request dto

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/CreateResidentRequest.java
@@ -0,0 +1,20 @@
+package com.jackalcode.resident_management_system.resident.dto;
+
+import com.jackalcode.resident_management_system.resident.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record CreateResidentRequest(
+        @NotBlank String firstName,
+        @NotBlank String lastName,
+        @NotNull LocalDate dateOfBirth,
+        @NotNull Gender gender,
+        @NotBlank String address,
+        @NotNull LocalDate admissionDate,
+        String primaryPhoneNumber,
+        String nhsNumber,
+        @NotBlank String allergies
+) {
+}
