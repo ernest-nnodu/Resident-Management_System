db2374c534c986724f639fafc564fe9cc70ed3bb
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-19 22:14:38 +0000

    Implement get residents method in resident service

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentService.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentService.java
@@ -1,4 +1,10 @@
 package com.jackalcode.resident_management_system.resident;
 
+import com.jackalcode.resident_management_system.resident.dto.ResidentResponse;
+
+import java.util.List;
+
 public interface ResidentService {
+
+    List<ResidentResponse> getResidents();
 }
--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentServiceImpl.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentServiceImpl.java
@@ -1,7 +1,28 @@
 package com.jackalcode.resident_management_system.resident;
 
+import com.jackalcode.resident_management_system.resident.dto.ResidentResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ResidentServiceImpl implements ResidentService {
+
+    private final ResidentRepository residentRepository;
+    private final ModelMapper mapper;
+
+    public ResidentServiceImpl(ResidentRepository residentRepository, ModelMapper mapper) {
+        this.residentRepository = residentRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<ResidentResponse> getResidents() {
+        List<Resident> residents = residentRepository.findAll();
+
+        return residents.stream()
+                .map(resident -> mapper.map(resident, ResidentResponse.class))
+                .toList();
+    }
 }
