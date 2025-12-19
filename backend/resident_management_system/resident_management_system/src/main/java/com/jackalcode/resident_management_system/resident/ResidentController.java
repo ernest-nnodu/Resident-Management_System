b9a51f2b9c11a6372e20e242a5d8754a4d36d0d0
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-19 22:21:42 +0000

    Add resident controller

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentController.java
@@ -0,0 +1,9 @@
+package com.jackalcode.resident_management_system.resident;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/residents")
+public class ResidentController {
+}
