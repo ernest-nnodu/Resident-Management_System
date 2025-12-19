6f057e50f603354c324281873f2574628df4a955
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-19 21:47:29 +0000

    Add resident service

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentService.java
@@ -0,0 +1,4 @@
+package com.jackalcode.resident_management_system.resident;
+
+public interface ResidentService {
+}
---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jackalcode.resident_management_system.resident;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResidentServiceImpl implements ResidentService {
+}
