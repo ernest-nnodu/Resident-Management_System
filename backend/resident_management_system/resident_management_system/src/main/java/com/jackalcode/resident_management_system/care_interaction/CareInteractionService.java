06125896c1953c539b50ad3c4038f3e6670a47d5
ernest-nnodu <ernest.nnodu@outlook.com>
2025-12-31 23:40:25 +0000

    Implement create care interaction method in care interaction service

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/CareInteractionService.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/CareInteractionService.java
@@ -1,6 +1,7 @@
 package com.jackalcode.resident_management_system.care_interaction;
 
 import com.jackalcode.resident_management_system.care_interaction.dto.CareInteractionResponse;
+import com.jackalcode.resident_management_system.care_interaction.dto.CreateCareInteractionRequest;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +9,5 @@ import java.util.UUID;
 public interface CareInteractionService {
 
     List<CareInteractionResponse> getCareInteractions(UUID residentId);
+    CareInteractionResponse createCareInteraction(UUID residentId, CreateCareInteractionRequest request);
 }
--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/CareInteractionServiceImpl.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/CareInteractionServiceImpl.java
@@ -1,8 +1,11 @@
 package com.jackalcode.resident_management_system.care_interaction;
 
 import com.jackalcode.resident_management_system.care_interaction.dto.CareInteractionResponse;
+import com.jackalcode.resident_management_system.care_interaction.dto.CreateCareInteractionRequest;
 import com.jackalcode.resident_management_system.exception.ResidentNotFoundException;
+import com.jackalcode.resident_management_system.resident.Resident;
 import com.jackalcode.resident_management_system.resident.ResidentRepository;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +41,27 @@ public class CareInteractionServiceImpl implements CareInteractionService {
         return careInteractions.stream()
                 .map(this::mapToResponse)
                 .toList();
+    }
+
+    @Override
+    @Transactional
+    public CareInteractionResponse createCareInteraction(UUID residentId, CreateCareInteractionRequest request) {
+
+        //Get resident with resident id, throw exception if resident id invalid
+        Resident resident = residentRepository.findById(residentId).orElseThrow(
+                () -> new ResidentNotFoundException("Resident not found with id: " + residentId));
+
+        //Map request to care interaction entity
+        CareInteraction careInteractionToSave = modelMapper.map(request, CareInteraction.class);
+
+        //Set resident in care interaction entity
+        careInteractionToSave.setResident(resident);
+
+        //Save care interaction entity to database
+        CareInteraction savedCareInteraction = careInteractionRepository.save(careInteractionToSave);
+
+        //Map saved care interaction entity to care interaction response and return
+        return mapToResponse(savedCareInteraction);
     }
 
     private CareInteractionResponse mapToResponse(CareInteraction careInteraction) {
