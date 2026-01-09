338aba3e132c346e56d5d3e1baaff560b515172d
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-09 19:25:04 +0000

    Implement get active support plans endpoint in support plan controller

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/support_plan/SupportPlanController.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/support_plan/SupportPlanController.java
@@ -1,6 +1,14 @@
 package com.jackalcode.resident_management_system.support_plan;
 
+import com.jackalcode.resident_management_system.support_plan.dto.SupportPlanSummaryResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class SupportPlanController {
@@ -9,5 +17,13 @@ public class SupportPlanController {
 
     public SupportPlanController(SupportPlanService supportPlanService) {
         this.supportPlanService = supportPlanService;
+    }
+
+    @GetMapping(path = "/api/v1/residents/{residentId}/support-plans")
+    public ResponseEntity<List<SupportPlanSummaryResponse>> getActiveSupportPlans(@PathVariable UUID residentId) {
+
+        List<SupportPlanSummaryResponse> supportPlanSummaries = supportPlanService.getActiveSupportPlans(residentId);
+
+        return new ResponseEntity<>(supportPlanSummaries, HttpStatus.OK);
     }
 }
