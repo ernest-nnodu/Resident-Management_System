da4328f0759fcfdd4f72d9d48d0cb686c6f24f0d
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-10 19:09:25 +0000

    Add support plan not found exception

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/exception/SupportPlanNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jackalcode.resident_management_system.exception;
+
+public class SupportPlanNotFoundException extends RuntimeException {
+
+    public SupportPlanNotFoundException(String message) {
+        super(message);
+    }
+}
