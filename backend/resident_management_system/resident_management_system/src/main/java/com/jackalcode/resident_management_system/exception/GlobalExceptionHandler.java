ccf4c93bd32490f6041139072581c255feade28b
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-14 18:21:10 +0000

    Add general exception handler

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/exception/GlobalExceptionHandler.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.w3c.dom.stylesheets.LinkStyle;
 
@@ -76,5 +78,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 }
         );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleGeneralException(Exception ex, HttpServletRequest request) {
+
+        return new ApiError("BAD_REQUEST", ex.getMessage(), request.getRequestURI());
     }
 }
