b0685e552f579b5c53990fc154c997c4d10c677c
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-16 12:29:47 +0000

    Add general API definiton

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/ResidentManagementSystemApplication.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/ResidentManagementSystemApplication.java
@@ -1,9 +1,19 @@
 package com.jackalcode.resident_management_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Resident Management REST API Documentation",
+				summary = "Spring Boot REST API documentation for Resident Management System",
+				version = "v1.0",
+				description = "API Provides CRUD Operations for residents, care interactions, and support plans"
+		)
+)
 public class ResidentManagementSystemApplication {
 
 	public static void main(String[] args) { SpringApplication.run(ResidentManagementSystemApplication.class, args);
