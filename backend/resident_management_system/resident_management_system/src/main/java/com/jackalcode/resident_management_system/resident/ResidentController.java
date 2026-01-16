e1e7e4b4d4fdb1fcd2ae47c80af355e78347eb79
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-16 18:57:53 +0000

    Customize api documentation for resident REST Api

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentController.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/ResidentController.java
@@ -3,6 +3,9 @@ package com.jackalcode.resident_management_system.resident;
 import com.jackalcode.resident_management_system.resident.dto.CreateResidentRequest;
 import com.jackalcode.resident_management_system.resident.dto.ResidentResponse;
 import com.jackalcode.resident_management_system.resident.dto.UpdateResidentRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Resident REST API", description = "Provides CRUD operations for managing residents")
 @RestController
 @RequestMapping("/api/v1/residents")
 public class ResidentController {
@@ -21,6 +25,8 @@ public class ResidentController {
         this.residentService = residentService;
     }
 
+    @Operation(summary = "Retrieve residents", description = "Retrieves All residents")
+    @ApiResponse(responseCode = "200", description = "Residents retrieved successfully")
     @GetMapping
     public ResponseEntity<List<ResidentResponse>> getResidents() {
         List<ResidentResponse> residentResponses = residentService.getResidents();
@@ -28,6 +34,8 @@ public class ResidentController {
         return new ResponseEntity<>(residentResponses, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create new resident", description = "Create and persist new resident")
+    @ApiResponse(responseCode = "201", description = "Resident created successfully")
     @PostMapping
     public ResponseEntity<ResidentResponse> createResident(@Valid @RequestBody CreateResidentRequest residentRequest) {
 
@@ -36,6 +44,8 @@ public class ResidentController {
         return new ResponseEntity<>(residentResponse, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Retrieve a resident", description = "Retrieve a resident by Id")
+    @ApiResponse(responseCode = "200", description = "Resident retrieved successfully")
     @GetMapping("/{residentId}")
     public ResponseEntity<ResidentResponse> getResident(@PathVariable UUID residentId) {
 
@@ -44,6 +54,8 @@ public class ResidentController {
         return new ResponseEntity<>(residentResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "Modify a resident", description = "Modify existing resident info")
+    @ApiResponse(responseCode = "200", description = "Resident updated successfully")
     @PutMapping("/{residentId}")
     public ResponseEntity<ResidentResponse> updateResident(@PathVariable UUID residentId,
                                                            @Valid @RequestBody UpdateResidentRequest request) {
@@ -53,6 +65,8 @@ public class ResidentController {
         return new ResponseEntity<>(residentResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a resident", description = "Deletes an existing resident")
+    @ApiResponse(responseCode = "209", description = "Resident deleted successfully")
     @DeleteMapping("/{residentId}")
     public ResponseEntity<HttpStatus> deleteResident(@PathVariable UUID residentId) {
 
