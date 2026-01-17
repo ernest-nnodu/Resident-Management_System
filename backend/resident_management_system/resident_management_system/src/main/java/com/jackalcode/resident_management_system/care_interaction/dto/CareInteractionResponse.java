69b156d867583a5fa4dbf2865d69cb1c5fb424f9
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-17 21:46:19 +0000

    Customize documentation for care interaction response DTO

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/dto/CareInteractionResponse.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/dto/CareInteractionResponse.java
@@ -1,23 +1,31 @@
 package com.jackalcode.resident_management_system.care_interaction.dto;
 
 import com.jackalcode.resident_management_system.care_interaction.CareInteractionType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Schema(description = "Care interaction response DTO")
 public record CareInteractionResponse(
 
+        @Schema(description = "Care interaction ID")
         UUID id,
 
+        @Schema(description = "Care interaction resident ID")
         UUID residentId,
 
+        @Schema(description = "Care interaction type")
         CareInteractionType type,
 
+        @Schema(description = "Care Interaction description")
         String description,
 
+        @Schema(description = "Care interaction record date")
         LocalDate recordedOn,
 
+        @Schema(description = "Care interaction record time")
         Instant recordedAt
 ) {
 }
