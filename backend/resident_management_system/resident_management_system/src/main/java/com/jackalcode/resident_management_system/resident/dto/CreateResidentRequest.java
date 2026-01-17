70cc6485606c68286a0a63b6d4b0baf00555d38a
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-17 21:06:50 +0000

    Customize documentation for create resident DTO

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/CreateResidentRequest.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/CreateResidentRequest.java
@@ -1,19 +1,21 @@
 package com.jackalcode.resident_management_system.resident.dto;
 
 import com.jackalcode.resident_management_system.resident.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
+@Schema(description = "Create resident request DTO")
 public record CreateResidentRequest(
-        @NotBlank(message = "First name is required") String firstName,
-        @NotBlank(message = "Last name is required") String lastName,
-        @NotNull(message = "Date of Birth is required") LocalDate dateOfBirth,
-        @NotNull(message = "Gender is required") Gender gender,
-        @NotBlank(message = "Address is required") String address,
-        String primaryPhoneNumber,
-        String nhsNumber,
-        @NotBlank(message = "Allergies are required") String allergies
+        @Schema(description = "Resident first name") @NotBlank(message = "First name is required") String firstName,
+        @Schema(description = "Resident last name") @NotBlank(message = "Last name is required") String lastName,
+        @Schema(description = "Resident Date of Birth")@NotNull(message = "Date of Birth is required") LocalDate dateOfBirth,
+        @Schema(description = "Resident Gender")@NotNull(message = "Gender is required") Gender gender,
+        @Schema(description = "Resident Address")@NotBlank(message = "Address is required") String address,
+        @Schema(description = "Resident phone number")String primaryPhoneNumber,
+        @Schema(description = "Resident NHS number")String nhsNumber,
+        @Schema(description = "Resident Allergies") @NotBlank(message = "Allergies are required") String allergies
 ) {
 }
