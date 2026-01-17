8176007f65c06ca54beb58ccc6fcfe8639bff273
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-17 21:40:16 +0000

    Customize documentation for update care interaction DTO

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/dto/UpdateCareInteractionRequest.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/care_interaction/dto/UpdateCareInteractionRequest.java
@@ -1,11 +1,15 @@
 package com.jackalcode.resident_management_system.care_interaction.dto;
 
 import com.jackalcode.resident_management_system.care_interaction.CareInteractionType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Update care interaction DTO")
 public record UpdateCareInteractionRequest(
 
+        @Schema(description = "Care interaction type")
         CareInteractionType type,
 
+        @Schema(description = "Care interaction description")
         String description
 ) {
 }
