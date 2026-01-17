a910d0ef1e60d8db25708668d6aca6579c335995
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-17 21:37:13 +0000

    Customize documentation for create care interaction DTO

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/CreateResidentRequest.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/CreateResidentRequest.java
@@ -9,13 +9,32 @@ import java.time.LocalDate;
 
 @Schema(description = "Create resident request DTO")
 public record CreateResidentRequest(
-        @Schema(description = "Resident first name") @NotBlank(message = "First name is required") String firstName,
-        @Schema(description = "Resident last name") @NotBlank(message = "Last name is required") String lastName,
-        @Schema(description = "Resident Date of Birth")@NotNull(message = "Date of Birth is required") LocalDate dateOfBirth,
-        @Schema(description = "Resident Gender")@NotNull(message = "Gender is required") Gender gender,
-        @Schema(description = "Resident Address")@NotBlank(message = "Address is required") String address,
-        @Schema(description = "Resident phone number")String primaryPhoneNumber,
-        @Schema(description = "Resident NHS number")String nhsNumber,
-        @Schema(description = "Resident Allergies") @NotBlank(message = "Allergies are required") String allergies
+        @Schema(description = "Resident first name")
+        @NotBlank(message = "First name is required") String firstName,
+
+        @Schema(description = "Resident last name")
+        @NotBlank(message = "Last name is required") String lastName,
+
+        @Schema(description = "Resident Date of Birth")
+        @NotNull(message = "Date of Birth is required")
+        LocalDate dateOfBirth,
+
+        @Schema(description = "Resident Gender")
+        @NotNull(message = "Gender is required")
+        Gender gender,
+
+        @Schema(description = "Resident Address")
+        @NotBlank(message = "Address is required")
+        String address,
+
+        @Schema(description = "Resident phone number")
+        String primaryPhoneNumber,
+
+        @Schema(description = "Resident NHS number")
+        String nhsNumber,
+
+        @Schema(description = "Resident Allergies")
+        @NotBlank(message = "Allergies are required")
+        String allergies
 ) {
 }
--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/UpdateResidentRequest.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/resident/dto/UpdateResidentRequest.java
@@ -9,13 +9,34 @@ import java.time.LocalDate;
 
 @Schema(description = "Update resident DTO")
 public record UpdateResidentRequest(
-        @Schema(description = "Resident first name") @NotBlank(message = "First name is required") String firstName,
-        @Schema(description = "Resident last name") @NotBlank(message = "Last name is required") String lastName,
-        @Schema(description = "Resident date of birth") @NotNull(message = "Date of Birth is required") LocalDate dateOfBirth,
-        @Schema(description = "Resident gender") @NotNull(message = "Gender is required") Gender gender,
-        @Schema(description = "Resident address") @NotBlank(message = "Address is required") String address,
-        @Schema(description = "Resident phone number") String primaryPhoneNumber,
-        @Schema(description = "Resident NHS number") String nhsNumber,
-        @Schema(description = "Resident allergies") @NotBlank(message = "Allergies are required") String allergies
+        @Schema(description = "Resident first name")
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @Schema(description = "Resident last name")
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @Schema(description = "Resident date of birth")
+        @NotNull(message = "Date of Birth is required")
+        LocalDate dateOfBirth,
+
+        @Schema(description = "Resident gender")
+        @NotNull(message = "Gender is required")
+        Gender gender,
+
+        @Schema(description = "Resident address")
+        @NotBlank(message = "Address is required")
+        String address,
+
+        @Schema(description = "Resident phone number")
+        String primaryPhoneNumber,
+
+        @Schema(description = "Resident NHS number")
+        String nhsNumber,
+
+        @Schema(description = "Resident allergies")
+        @NotBlank(message = "Allergies are required")
+        String allergies
 ) {
 }
