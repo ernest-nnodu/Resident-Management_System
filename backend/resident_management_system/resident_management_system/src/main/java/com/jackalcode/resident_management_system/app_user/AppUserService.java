051760e824116d7a81a855aa6e1596703177fc36
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-23 18:28:00 +0000

    Implement get all user method in app user service

--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserService.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserService.java
@@ -1,4 +1,10 @@
 package com.jackalcode.resident_management_system.app_user;
 
+import com.jackalcode.resident_management_system.app_user.dto.AppUserSummaryResponse;
+
+import java.util.List;
+
 public interface AppUserService {
+
+    List<AppUserSummaryResponse> getUsers();
 }
--- backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserServiceImpl.java
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserServiceImpl.java
@@ -1,7 +1,39 @@
 package com.jackalcode.resident_management_system.app_user;
 
+import com.jackalcode.resident_management_system.app_user.dto.AppUserSummaryResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AppUserServiceImpl implements AppUserService {
+
+    private final AppUserRepository userRepository;
+
+    public AppUserServiceImpl(AppUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<AppUserSummaryResponse> getUsers() {
+
+        List<AppUser> users = userRepository.findAll();
+
+        return users.stream()
+                .map(this::mapToSummaryResponse)
+                .toList();
+    }
+
+    private AppUserSummaryResponse mapToSummaryResponse(AppUser user) {
+
+        return new AppUserSummaryResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole(),
+                user.isEnabled()
+        );
+
+    }
 }
