e6802f80168b6d4d4f407b3d811e30a7e67867f2
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-23 18:12:25 +0000

    Add app user service

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserService.java
@@ -0,0 +1,4 @@
+package com.jackalcode.resident_management_system.app_user;
+
+public interface AppUserService {
+}
---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jackalcode.resident_management_system.app_user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserServiceImpl implements AppUserService {
+}
