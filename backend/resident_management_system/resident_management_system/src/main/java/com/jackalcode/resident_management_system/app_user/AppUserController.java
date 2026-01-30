dbe6215e1072e6b597a0a29102c9578fa7c1d5db
ernest-nnodu <ernest.nnodu@outlook.com>
2026-01-30 21:15:01 +0000

    Add app user controller

---
+++ backend/resident_management_system/resident_management_system/src/main/java/com/jackalcode/resident_management_system/app_user/AppUserController.java
@@ -0,0 +1,22 @@
+package com.jackalcode.resident_management_system.app_user;
+
+import com.jackalcode.resident_management_system.app_user.dto.AppUserSummaryResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class AppUserController {
+
+    private final AppUserService appUserService;
+
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+}
